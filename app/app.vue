0dbcbf4f057f1a372283ef5858f98fe1601872d0
Panha <sokunpanha.panha@gmail.com>
2026-01-29 13:06:17 +0700

    structure layout

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,22 @@
+<script setup>
+const route = useRoute()
+const layout = computed(() => {
+  if (route.path.startsWith('/admin')) {
+    return 'admin-layout'
+  } else if (route.path.startsWith('/pos')) {
+    return 'pos-layout'
+  }
+  else {
+    return 'default'
+  }
+})
+
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <UApp>
+    <NuxtLayout :name="layout">
+      <NuxtPage />
+    </NuxtLayout>
+  </UApp>
 </template>
---
+++ app/layouts/pos-layout.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1>POS Layout</h1>
+    <slot/>
+</template>
