8fa8bd43074d1169b0e1581486f01a4db502513b
Panha <sokunpanha.panha@gmail.com>
2026-02-07 13:02:25 +0700

    feat: add OrderSummary reusable component

---
+++ app/components/ui/OrderSummary.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+const props = defineProps<{
+  totalItems: number;
+  subTotal: number;
+  discount: number;
+  total: number;
+}>();
+const { t } = useI18n();
+</script>
+<template>
+     <div class="space-y-2 text-sm">
+      <div class="flex justify-between">
+        <span>{{ t("label.items") }}</span>
+        <span>{{ props.totalItems }}</span>
+      </div>
+
+      <div class="flex justify-between">
+        <span>{{ t("label.subtotal") }}</span>
+        <span>${{ props.subTotal.toFixed(2) }}</span>
+      </div>
+
+      <div
+        v-if="props.discount > 0"
+        class="flex justify-between text-green-600"
+      >
+        <span>{{ t("label.discount") }}</span>
+        <span>- ${{ props.discount.toFixed(2) }}</span>
+      </div>
+
+      <div class="flex justify-between font-semibold text-base">
+        <span>{{ t("label.total") }}</span>
+        <span>${{ props.total.toFixed(2) }}</span>
+      </div>
+    </div>-
+</template>
